a2e9d1213215e15f05395d84d74c7a22a0b35658
Albina Vivdenko <albinka3322@gmail.com>
2024-11-10 18:49:38 +0100

    bar

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux';
 import { logOut } from '../../redux/auth/operations';
 import { useAuth } from '../../hooks';
 import css from './UserMenu.module.css';
+import myImage from './img/defaultavatar.jpg';
 
 export const UserMenu = () => {
   const dispatch = useDispatch();
@@ -10,6 +11,14 @@ export const UserMenu = () => {
   return (
     <div className={css.nav}>
       <p className={css.username}>Welcome, {user.name}</p>
+      <img
+        className={css.avatar}
+        src={myImage}
+        alt="photo user"
+        width="30"
+        height="30"
+      />
+
       <button
         className={css.button}
         type="button"
