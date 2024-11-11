49000b6135b7ec0dc7d5ba314e2baab3bb8d29f2
AlbinaViv <133263182+AlbinaViv@users.noreply.github.com>
2024-11-11 11:19:30 +0000

    Deploying to gh-pages from @ AlbinaViv/project-for-arla@00f705ad24ed9defbc2d709d000cb2d8ceecb8b6 🚀

---
+++ assets/DocumentTitle-TNDFlM2G.js
@@ -0,0 +1,2 @@
+import{j as t,H as r}from"./index-JjlajPGm.js";function i({children:e}){return t.jsx(r,{children:t.jsx("title",{children:e})})}export{i as D};
+//# sourceMappingURL=DocumentTitle-TNDFlM2G.js.map
